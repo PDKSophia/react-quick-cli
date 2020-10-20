d794dbd12c31e3324a95aaab3a89d14e8d9e9a11
彭道宽 <pengdaokuan@cvte.com>
2020-10-20 10:33:35 +0800

    [feature][what]初始化脚手架

---
+++ README.md
@@ -0,0 +1,15 @@
+# react-quick-cli
+
+快速开发 react 第三方包脚手架
+
+## 本地开发
+
+```bash
+npm run start
+```
+
+## 打包
+
+```bash
+npm run build
+```
---
+++ src/components/Main/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Main extends React.Component {
+  render() {
+    return <div>react-quick-cli</div>
+  }
+}
+
+export default Main
